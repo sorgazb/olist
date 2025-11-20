b0e901b938ac6a89ee29102e81f138c75ed17aed
sorgazb <sorgazb@gmail.com>
2025-11-20 17:08:13 +0100

    aa

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import pandas as pd
-st.write("Versión de Streamlit:", st.__version__)
+pages = {
+    "Home": [
+        st.Page("main.py", title="Home", icon="🏠")
+    ],
+    "Conoce los datos": [
+        st.Page("pages/data.py", title="Datos de las tablas", icon="📊")
+    ], 
+    "Análisis de datos por estado": [
+        st.Page("pages/clientesEstado.py", title="Clientes por estado", icon="👥"),
+        st.Page("pages/graficoMedidasEstado.py", title="Gráfico por estado", icon="📈"),
+        st.Page("pages/resenas.py", title="Análisis de reseñas", icon="⭐"),
+        st.Page("pages/mapa.py", title="Mapa", icon="🌎")
+    ],
+    "Análisis de datos por ciudad": [
+        st.Page("pages/clienteCiudad.py", title="Clientes por ciudad",  icon="👥"),
+        st.Page("pages/graficoCiudad.py", title="Gráficos por ciudad", icon="📉")
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
 
 col1,col2 = st.columns([0.5,2])
 with col1:
@@ -74,24 +94,4 @@ with col:
 with colm:
     st.page_link("pages/data.py", label="Datos de las tablas", icon="➡️")
 
-pages = {
-    "Home": [
-        st.Page("main.py", title="Home", icon="🏠")
-    ],
-    "Conoce los datos": [
-        st.Page("pages/data.py", title="Datos de las tablas", icon="📊")
-    ], 
-    "Análisis de datos por estado": [
-        st.Page("pages/clientesEstado.py", title="Clientes por estado", icon="👥"),
-        st.Page("pages/graficoMedidasEstado.py", title="Gráfico por estado", icon="📈"),
-        st.Page("pages/resenas.py", title="Análisis de reseñas", icon="⭐"),
-        st.Page("pages/mapa.py", title="Mapa", icon="🌎")
-    ],
-    "Análisis de datos por ciudad": [
-        st.Page("pages/clienteCiudad.py", title="Clientes por ciudad",  icon="👥"),
-        st.Page("pages/graficoCiudad.py", title="Gráficos por ciudad", icon="📉")
-    ]
-}
 
-pg = st.navigation(pages)
-pg.run()
